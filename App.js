200533278cf4f6a4166cf7e02731883b85e79162
Deschant Kounou <kounoudeschant@gmail.com>
2020-03-03 16:12:11 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = (goalTittle) => {
+    setCourseGoals((currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTittle }]));
+  };
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+});
